5ac7ad2158e9880d87164956b95ec0f31dac764d
JARVIS-Mocap <jarvismocap@gmail.com>
2022-06-01 15:46:49 +0200

    updated opencv lib on 1804, minor fix CMake and updated deployment version stuff

--- deployment/deb_packages/JARVIS-AcquisitionTool_0.3-1_amd64_2004/usr/local/lib/JARVIS-AcquisitionTool/change_rpath.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for filename in OpenCV/*; do
-    patchelf --set-rpath '/usr/local/lib/JARVIS-AcquisitionTool/OpenCV' $filename
-done
--- deployment/deploy_Ubuntu_2004.sh
+++ deployment/deploy_Ubuntu_2004.sh
@@ -4,5 +4,5 @@ cmake .. -DUSE_CUDA=True
 make -j12
 make install
 cd ../deployment
-dpkg-deb --build --root-owner-group deb_packages/JARVIS-AcquisitionTool_0.3-1_amd64_2004
-mv deb_packages/JARVIS-AcquisitionTool_0.3-1_amd64_2004.deb .
+dpkg-deb --build --root-owner-group deb_packages/JARVIS-AcquisitionTool_1.0-1_amd64_2004
+mv deb_packages/JARVIS-AcquisitionTool_1.0-1_amd64_2004.deb .
