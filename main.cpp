366e8175161cf67fd2da28306df366acbcc69699
timohueser <timo.hueser@gmail.com>
2022-08-16 12:19:16 +0200

    added libusb test stuff

--- main.cpp
+++ main.cpp
@@ -23,7 +23,74 @@
 Q_DECLARE_METATYPE(QList<int>)
 Q_DECLARE_METATYPE(statusType)
 
+#include <libusb-1.0/libusb.h>
+
+static void print_devs(libusb_device **devs) {
+    libusb_device *dev;
+    int i = 0, j = 0;
+    uint8_t path[8];
+
+    while ((dev = devs[i++]) != NULL) {
+        struct libusb_device_descriptor desc;
+        int r = libusb_get_device_descriptor(dev, &desc);
+        if (r < 0) {
+            fprintf(stderr, "failed to get device descriptor");
+            return;
+        }
+
+        printf("%04x:%04x (bus %d, device %d)", desc.idVendor, desc.idProduct,
+               libusb_get_bus_number(dev), libusb_get_device_address(dev));
+
+        libusb_device_handle *dev_handle;
+
+        r = libusb_get_port_numbers(dev, path, sizeof(path));
+        if (r > 0) {
+            printf(" path: %d", path[0]);
+            for (j = 1; j < r; j++)
+                printf(".%d", path[j]);
+        }
+        printf("\n");
+        int err = libusb_open(dev, &dev_handle);
+        if (!err) {
+            unsigned char manufacturer[200];
+            libusb_get_string_descriptor_ascii(dev_handle, desc.iManufacturer,
+                                               manufacturer, 200);
+            printf("Manufacturer: %s \n", manufacturer);
+
+            unsigned char name[200];
+            libusb_get_string_descriptor_ascii(dev_handle, desc.iProduct,
+                                               name, 200);
+            printf("Name: %s \n", name);
+
+            unsigned char serialNumber[200];
+            libusb_get_string_descriptor_ascii(dev_handle, desc.iSerialNumber,
+                                               serialNumber, 200);
+            printf("SerialNumber: %s \n", serialNumber);
+        }
+        printf("\n");
+    }
+}
+
 int main(int argc, char **argv) {
+    libusb_device **devs;
+    int r;
+    ssize_t cnt;
+
+    r = libusb_init(NULL);
+    if (r < 0)
+        return r;
+
+    cnt = libusb_get_device_list(NULL, &devs);
+    if (cnt < 0) {
+        libusb_exit(NULL);
+        return (int)cnt;
+    }
+
+    print_devs(devs);
+    libusb_free_device_list(devs, 1);
+
+    libusb_exit(NULL);
+
     qSetMessagePattern(QStringLiteral("%{time}"
                                       //"%{appname}"
                                       ": ["
