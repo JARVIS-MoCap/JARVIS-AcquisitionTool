c90bbe794b99e1467d3eba851a96b0a732131b43
Padok <padok@disroot.org>
2022-09-01 12:02:32 +0200

    dont change byteorder

--- src/arduinoserialpeer.hpp
+++ src/arduinoserialpeer.hpp
@@ -7,14 +7,6 @@
 #include "serialinterface.hpp"
 #include <QMutex>
 
-#define htons(x) (((x) << 8 & 0xFF00) | ((x) >> 8 & 0x00FF))
-#define ntohs(x) htons(x)
-
-#define htonl(x)                                                               \
-    (((x) << 24 & 0xFF000000UL) | ((x) << 8 & 0x00FF0000UL) |                  \
-     ((x) >> 8 & 0x0000FF00UL) | ((x) >> 24 & 0x000000FFUL))
-#define ntohl(x) htonl(x)
-
 #define SERIAL_PEER_MAX_BUFFER_SIZE (0xFF)
 #define SERIAL_PEER_COBS_OVERHEAD (1)
 #define SERIAL_PEER_DELIMITER_OVERHEAD (1)
