1ce753ba034311e05d7dfbf0c86c5fe84e5c6daf
jarvis <jarvismovap@gmail.com>
2023-12-15 12:31:11 +0100

    bumped release to v1.2

--- deployment/deploy_Ubuntu_2004.sh
+++ deployment/deploy_Ubuntu_2004.sh
@@ -1,8 +1,8 @@
 mkdir ../build
 cd ../build
-cmake .. -DUSE_CUDA=True -DCMAKE_BUILD_TYPE=RELEASE
+cmake .. -DUBUNTU_VERSION=2004 -DUSE_CUDA=True -DCMAKE_BUILD_TYPE=RELEASE
 cmake --build . --parallel 16
 cmake --install .
 cd ../deployment
-dpkg-deb --build --root-owner-group deb_packages/JARVIS-AcquisitionTool_1.1-1_amd64_2004
-mv deb_packages/JARVIS-AcquisitionTool_1.1-1_amd64_2004.deb .
+dpkg-deb --build --root-owner-group deb_packages/JARVIS-AcquisitionTool_1.2-1_amd64_2004
+mv deb_packages/JARVIS-AcquisitionTool_1.2-1_amd64_2004.deb .
